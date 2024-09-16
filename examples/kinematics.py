204e9206077ef8de57f932b030a23f18117d3fb7
simeon <simkaned@gmail.com>
2024-09-16 10:30:38 +0300

    modified example

--- examples/kinematics.py
+++ examples/kinematics.py
@@ -12,5 +12,14 @@ data.motor.position = ca.SX.sym("theta")
 print(model)
 print(data)
 print(contraction(model, data))
-print(ca.jacobian(contraction(model, data), data.motor.position))
-print(jacobian(model, data))
+# generate function to test:
+
+model.kinematic.length = 0.2
+model.kinematic.radius = 1e-3
+cas_jacobian = ca.jacobian(contraction(model, data), data.motor.position)
+cas_jacobian_func = ca.Function("cas_jacobian", [data.motor.position], [cas_jacobian])
+analytical_jacobian = jacobian(model, data)
+analytical_jacobian_func = ca.Function("analytical_jacobian", [data.motor.position], [analytical_jacobian])
+
+print(analytical_jacobian_func(100))
+print(cas_jacobian_func(100))
