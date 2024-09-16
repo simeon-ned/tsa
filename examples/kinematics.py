5bfec42b5ff797820921184457677de94c0b0da5
simeon <simkaned@gmail.com>
2024-09-16 10:48:29 +0300

    modified dynamics and data

--- examples/kinematics.py
+++ examples/kinematics.py
@@ -5,13 +5,17 @@ import casadi as ca
 model = Model()
 model.kinematic.length = ca.SX.sym("L")
 model.kinematic.radius = ca.SX.sym("r")
+print(model)
 
 data = Data()
 data.motor.position = ca.SX.sym("theta")
-
-print(model)
 print(data)
-print(contraction(model, data))
+
+x = contraction(model, data)
+print(x)
+# This will also populate the data.load.position
+print(x == data.load.position)
+
 # generate function to test:
 
 model.kinematic.length = 0.2
@@ -19,7 +23,10 @@ model.kinematic.radius = 1e-3
 cas_jacobian = ca.jacobian(contraction(model, data), data.motor.position)
 cas_jacobian_func = ca.Function("cas_jacobian", [data.motor.position], [cas_jacobian])
 analytical_jacobian = jacobian(model, data)
-analytical_jacobian_func = ca.Function("analytical_jacobian", [data.motor.position], [analytical_jacobian])
 
+# This will also populate the data.motor.jacobian
+print(analytical_jacobian == data.motor.jacobian)
+
+analytical_jacobian_func = ca.Function("analytical_jacobian", [data.motor.position], [analytical_jacobian])
 print(analytical_jacobian_func(100))
 print(cas_jacobian_func(100))
